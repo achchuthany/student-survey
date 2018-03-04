09180057294961b9fe0f3f8f6ce2164ff8414b72
HOMEPC-ACHCHUTH\ACHCHUTHAN <achch1990@gmail.com>
2018-03-04 13:30:59 +0530

    Config file addedd

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+date_default_timezone_set("Asia/Colombo"); 
+
+//define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST')); // 127.0.250.1
+//define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT')); // 3306
+//define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME')); // admin
+//define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD')); // 8ddTnst22X3Y
+//define('DB_NAME','survey');
+//define('DB_SOCKET', getenv('OPENSHIFT_MYSQL_DB_SOCKET')); //
+//define('DB_STRING', getenv('OPENSHIFT_MYSQL_DB_URL')); // 
+
+
+define('DB_HOST','localhost' ); // 127.0.250.1
+define('DB_USER','root' ); // admin
+define('DB_PASS','' ); // 8ddTnst22X3Y
+define('DB_NAME','survey');
+
+$now = date("Y-m-d H:i:s");
+$con = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME) or die("Failed to connect to MySQL: ".mysqli_connect_error());
